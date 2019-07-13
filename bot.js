dc43248bbca717f53f5b03b534aec396fe562a8f
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-13 07:53:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -426,31 +426,6 @@ message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {
  })
     }});
 
-client.on('message', async message => {
-  let mention = message.mentions.members.first();
-let command = message.content.split(" ")[0];
-   command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);	 
-	if (message.content === ".unmute") {
-    if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.sendMessage("**You Donot HavePermission Mute_Members**").then(m => m.delete(5000));
-if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**I donot Have Permission Mute_Members**").then(msg => msg.delete(6000))
-
-  let kinggamer = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-     if(!kinggamer) return message.channel.send('Mention Someone')
-
-
-  let role = message.guild.roles.find (r => r.name === "Muted");
-  
-  if(!role || !kinggamer.roles.has(role.id)) return message.channel.sendMessage(`**:information_source:${mention.user.username} لقد تم فك الميوت عنه مسبقا**`)
-
-  await kinggamer.removeRole(role) 
-  message.channel.sendMessage(`**:white_check_mark: ${mention.user.username}  Unmute> **`);
-  
-  return;
-
-  }
-
-});
 
 
 
