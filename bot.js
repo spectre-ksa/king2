ea3f6ea48c345c630faa1fccdc7b9a30916ab3a4
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2020-04-16 00:44:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ client.on('ready',  () => {
 
   
 
-const developers = ["434642311591493632","id"]
+const developers = ["492756531884851210","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
